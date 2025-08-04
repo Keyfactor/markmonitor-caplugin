cc2b31b875b77070bd69f7f5bbc6868746e24534
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2025-08-04 12:11:55 -0700

    fix(proj): Rename to `caplugin`

--- markmonitor-caplugin/Properties/AssemblyInfo.cs
+++ markmonitor-caplugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("markmonitor-cagateway")]
+[assembly: AssemblyTitle("markmonitor-caplugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("markmonitor-cagateway")]
+[assembly: AssemblyProduct("markmonitor-caplugin")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
